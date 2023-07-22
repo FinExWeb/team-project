e48a0aee0db74d35be792d47dc10d2a1791e03cc
FinExWeb <akhmedovweb@gmail.com>
2023-07-22 18:18:26 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div>Evrything update!!!</div>
+    <>
+      <div className="container">
+        <div className="navbar">
+          <Navbar />
+        </div>
+      </div>
+    </>
   )
 }
 
