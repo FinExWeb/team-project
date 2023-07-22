41c747de7648e4b1496da45e9a0acd1e9334a7d5
FinExWeb <akhmedovweb@gmail.com>
2023-07-22 18:22:19 +0500

    add

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import './Navbar.css'
 function Navbar() {
     return (
         <>
             <div className="navbar">
-
+                navbar
             </div>
         </>
     )
 }
 
-export default Navbar
+export default Navbar;
